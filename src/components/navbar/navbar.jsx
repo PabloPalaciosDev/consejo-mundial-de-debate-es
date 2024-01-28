67c1ed57be4ebc6060dd6800cdc68d0c5562a340
Pablo Palacios <72522406+PabloPalaciosDev@users.noreply.github.com>
2024-01-28 16:17:06 -0500

    .

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -52,7 +52,7 @@ function NavBar() {
 
     return (
         <div className='navbar'>
-            <img src="/src/assets/images/logo-cmd-sin-fondo.png" alt="logo-cmd" width={200} />
+            <img src="./assets/images/logo-cmd-sin-fondo.png" alt="logo-cmd" width={200} />
             <nav className="card">
                 <Menubar model={items} className='menu-bar-container' />
             </nav>

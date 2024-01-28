513d49c536f6f76a7a03f53831b88feea7b922ba
Pablo Palacios <72522406+PabloPalaciosDev@users.noreply.github.com>
2024-01-28 16:10:56 -0500

    manejo de rutas arreglado xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
 import './App.css'
 import { PrimeReactProvider } from 'primereact/api';
 import NavBar from './components/navbar/navbar';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import routes from './routes';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom';
+//PAGINAS 
+import LandingPage from './pages/landing/landing';
+import SobreNosotros from './pages/sobre-nosotros';
+import ProyectosCmd from './pages/proyectos-cmd';
+import BaseDeDatos from './pages/base-de-datos-cmd';
+
+
+const routes = [
+  { path: '/', component: <LandingPage/> },
+  { path: '/sobre-nosotros', component: <SobreNosotros/>},
+  { path: '/proyectos-cmd', component: <ProyectosCmd/>},
+  { path: '/base-de-datos', component: <BaseDeDatos/>},
+];
+
 
 function App() {
 
   return (
     <>
       <PrimeReactProvider>
-    <Router>
-      <>
         <NavBar />
-        <Routes>
-          {routes.map((route) => (
-            <Route key={route.path} path={route.path} element={<route.component />} />
-          ))}
-        </Routes>
-      </>
-    </Router>
+        <BrowserRouter>
+          <Routes>
+            {routes.map((route) => (
+              <Route key={route.path} path={route.path} element={route.component}>
+              </Route>
+            ))}
+          </Routes>
+        </BrowserRouter>
       </PrimeReactProvider>
     </>
   )
